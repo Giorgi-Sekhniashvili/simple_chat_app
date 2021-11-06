e46478e9c19cdc6940178e7dc1f58b480e91b0d8
Giorgi-Sekhniashvili <giorgisekhniashvili99@gmail.com>
2021-11-07 00:56:05 +0400

    chat creation and messages view

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -6,5 +6,5 @@ app_name = 'chat'
 
 urlpatterns = [
     path('', IndexPageView.as_view(), name='index'),
-    path('chat/<int:user2_id>/', PrivateChatView.as_view(), name='chat')
+    path('chat/<str:chat_name>/', PrivateChatView.as_view(), name='chat')
 ]
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,38 +1,59 @@
+from hashlib import sha256
+
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractBaseUser
 from django.db.models import Q
 from django.http import HttpResponseForbidden
-from django.shortcuts import render
-from django.views.generic import TemplateView, View
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
 from .models import PrivateChat
 
-UserModel = get_user_model()
+UserModel: AbstractBaseUser = get_user_model()
 
 
-class IndexPageView(TemplateView):
+class IndexPageView(View):
     template_name = 'chat/index.html'
 
-    def get_context_data(self, **kwargs):
+    def get(self, request):
         user = self.request.user
         if user.is_authenticated:
-            context = {'users': UserModel.objects.filter(~Q(id=user.id))}
+            context = {
+                'users': UserModel.objects.filter(~Q(id=user.id)),
+                'chats': PrivateChat.objects.filter(Q(user1=user) | Q(user2=user))
+            }
         else:
             context = {}
-        return context
+
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
+        user1_id = int(request.POST.get('user1_id'))
+        if request.user.id != user1_id:
+            return HttpResponseForbidden()
+        user2_id = int(request.POST.get('user2_id'))
+
+        user1 = UserModel.objects.get(id=user1_id)
+        user2 = UserModel.objects.get(id=user2_id)
+        encoded_chat_name1 = sha256(f'{user1.get_username()}{user2.get_username()}'.encode('utf-8')).hexdigest()
+        encoded_chat_name2 = sha256(f'{user2.get_username()}{user1.get_username()}'.encode('utf-8')).hexdigest()
+        try:
+            private_chat = PrivateChat.objects.get(encoded_chat_name=encoded_chat_name1)
+        except PrivateChat.DoesNotExist:
+            try:
+                private_chat = PrivateChat.objects.get(encoded_chat_name=encoded_chat_name2)
+            except PrivateChat.DoesNotExist:
+                private_chat = PrivateChat(encoded_chat_name=encoded_chat_name1, user1=user1, user2=user2)
+                private_chat.save()
+
+        return redirect(to='chat:chat', chat_name=private_chat.encoded_chat_name)
 
 
 class PrivateChatView(View):
-    def get(self, request, user2_id):
-        user1 = self.request.user
-
-        if not user1.is_authenticated:
-            return HttpResponseForbidden()
-
-        if user1.id == user2_id:
-            return HttpResponseForbidden()
-        # TODO create PrivateChat
-
-        private_chat = PrivateChat.objects.get_or_create(user1_id=user1.id, user2_id=user2_id)
-        print(private_chat)
-        context = {}
+    def get(self, request, chat_name):
+        private_chat = PrivateChat.objects.get(encoded_chat_name=chat_name)
+        context = {
+            'private_chat': private_chat,
+            'messages': private_chat.privatechatmessage_set
+        }
         return render(request, 'chat/private_chat.html', context=context)
