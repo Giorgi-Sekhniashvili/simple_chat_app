946c2661a0808bb5e6d81548759915dfc329c81d
Giorgi-Sekhniashvili <giorgisekhniashvili99@gmail.com>
2021-11-07 01:19:00 +0400

    staticfiles dir

--- simple_chat_app/settings.py
+++ simple_chat_app/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = []
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
